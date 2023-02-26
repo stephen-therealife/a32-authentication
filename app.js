45f16aefdd41b9333de1af7be0fff147b236ab09
Stephen Chong <stephen.xylon@gmail.com>
2023-02-26 20:03:30 +0800

    Encrypted by MD5 Hashes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 //Initial Setup
 
 require('dotenv').config()
-console.log(process.env)
-
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -23,11 +21,6 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
-})
-
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET_KEY,
-    encryptedFields: ["password"]
 })
 
 const User = new mongoose.model("User", userSchema)
@@ -54,7 +47,7 @@ app.post("/register", (req, res) => {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save((err) => {
@@ -70,7 +63,7 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
 
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne(
         {email: username},
