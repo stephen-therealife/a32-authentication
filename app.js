cf93c75389052f46b1e6308dc906c1462c8a9956
Stephen Chong <stephen.xylon@gmail.com>
2023-02-27 08:57:05 +0800

    Level 4 Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+
+const saltRounds = 10
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -45,17 +47,19 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    newUser.save((err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render("secrets")
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save((err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.render("secrets")
+            }
+        })
     })
 
 })
@@ -63,7 +67,7 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
 
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne(
         {email: username},
@@ -72,11 +76,13 @@ app.post("/login", (req, res) => {
                 console.log(err)
             } else {
                 if (foundUser) {
-                    if (foundUser.password === password) {
-                        res.render("secrets")
-                    } else {
-                        res.send("Wrong Password.")
-                    }
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if (result === true) {
+                            res.render("secrets")
+                        } else {
+                            res.send("Wrong password.")
+                        }
+                    })
                 }
             }
         }
